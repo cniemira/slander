3095c8ed2dc2eb32f4cc84cdff64e54b07a2f32c
Chris Niemira <chris.niemira@teamaol.com>
2016-10-21 13:20:49 -0400

    cleanup, ignore users, argparse

--- bot.py
+++ bot.py
@@ -8,9 +8,7 @@ from textwrap import TextWrapper
 
 from slackclient import SlackClient
 from terminaltables import AsciiTable
-#from veryprettytable import VeryPrettyTable, ALL
 
-logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
 
 # TODO:
@@ -19,99 +17,114 @@ log = logging.getLogger(__name__)
 
 messages = {
     'help' : """Standup management (from a channel or pvt group):
-`@{name} start`       Start a standup. I will message everyone in the channel
-`@{name} cancel`      Cancel a standup that has been started
-`@{name} publish`     Publish a standup that has been started
+  `@{name} start`       Start a standup. I will message everyone in the channel
+  `@{name} cancel`      Cancel a standup that has been started
+  `@{name} publish`     Publish a standup that has been started
  
 Status entry (via direct message):
-`d: [...]`     Something you have done since the last standup
-`b: [...]`     A blocker that prevented you from getting something done
-`g: [...]`     A goal for today
-`end`     When you've no more to say
-`show`     If you're too lazy to scroll back
-`skip`     Nothing for today, diposes of anything you've entered
-`reset [...]`     Reset a status category, or all
+  `d: [...]`     Something you have done since the last standup
+  `b: [...]`     A blocker that prevented you from getting something done
+  `g: [...]`     A goal for today
+  `end`     When you've no more to say
+  `show`     If you're too lazy to scroll back
+  `skip`     Nothing for today, diposes of anything you've entered
+  `reset [...]`     Reset a status category, or all
  
 Example:
- Group:
- <boss> @{name} start
- <{name}> group standup started.
+  Your group chat:
+    <boss> @{name} start
+    <{name}> group standup started.
  
- DM:
- <{name}>  @{name} Standup time for group!
- <you> d: finished documenting all the things
- <{name}> done: 1, blocked: 0, goals: 0
- <you> g: find more thing to document
- <{name}> done: 1, blocked: 0, goals: 1
- <you> end
- <{name}> Thanks; done: 1, blocked: 0, goals: 1
+  direct-message with me:
+    <{name}>  @{name} Standup started for group!
+    <you> d: finished documenting all the things
+    <{name}> done: 1, blocked: 0, goals: 0
+    <you> g: find more thing to document
+    <{name}> done: 1, blocked: 0, goals: 1
+    <you> end
+    <{name}> Thanks; done: 1, blocked: 0, goals: 1
  
- Group:
- <{name}> @you sat down.
- <boss> @{name} publish
- <{name}> ...
-""",
-
-    'started' : """Standup started for {standup.channel.name}!
- 
-Remember to enter:
- 1) Things you did since the last standup (`d: ...`)
- 2) What blockers you ran into (`b: ...`)
- 3) Your goals for today (`g: ...`)
- 
-Type `end` when you're finished, or `help` if you need.
+  Back in your group chat:
+    <{name}> @you sat down.
+    <boss> @{name} publish
+    <{name}> ...
 """,
 
     'next' : 'Another standup, for {standup.channel.name}, also started.',
 
     'no_standup' : 'No active standup.',
 
+    'pong' : 'pong',
+
     'preview': """Preview:
   
 {update}
 """,
 
-    'pong' : 'pong',
-
-    'standup_started' : '{standup.channel.name} standup started.',
-    'standup_ended' : '*{standup.channel.name} standup ended!',
-    'standup_cancelled' : '{standup.channel.name} standup cancelled.',
-    'standup_already' : '{standup.channel.name} standup already started.',
+    'reset_what': 'Reset what?',
 
     'sat_down': '{user.tag} sat down.',
 
-    'unknown_cmd' : 'Bad command. Did you mean `start` or `publish`?',
+    'standup_already': '{standup.channel.name} standup already started.',
+    'standup_cancelled': '{standup.channel.name} standup cancelled.',
+    'standup_empty': '*There are no users in the standup!*',
+    'standup_ended': '*{standup.channel.name} standup ended!',
+    'standup_for': '*Standup for {standup.channel.name}*',
+    'standup_started': '{standup.channel.name} standup started.',
+
+    'started' : """Standup started for {standup.channel.name}!
+Use `d: ...`, `b: ...`, `g: ...` to report tasks done, blockers, and goals.
+Type `end` when you're finished, or `help` if you need.
+""",
+
+    'unknown_cmd' : 'Bad command. Try `help` if you need it.',
+
+    'Done': 'Done',
+    'Blocked': 'Blocked',
+    'Goals': 'Goals',
+    'None': '*None*',
+    'Ok': 'Ok',
+    'Skipping': 'Skipping you',
+    'Thanks': 'Thanks',
     }
 
 
 commands = {
     'blocked': ('b:', 'blocked', 'block', 'stuck'),
+    'cancel': ('cancel'),
     'done': ('d:', 'did', 'finished', 'completed'),
     'end': ('end'),
+    'help': ('help'),
     'goals': ('g:', 'goal', 'will', 'shall'),
     'reset': ('reset'),
     'skip': ('skip', 'no'),
     'show': ('show', 'what'),
+    'start': ('start', 'begin'),
+    'ping': ('ping'),
+    'publish': ('publish'),
     }
 
 
 def _(key, **kwds):
+    if not key in messages:
+        log.error('Undefined message: "{}"'.format(key))
+        return ''
     return str(messages[key]).format(**kwds)
 
 
 class Standup(object):
     def __init__(self, channel):
-        log.debug('Standup initialized in {}'.format(channel.name))
-        #self.slack_client = slack_client
+        assert isinstance(channel, Channel)
         self.channel = channel
-        #self.channel = self.get_channel_info(channel)
         self.outstanding = 0
         self.updates = {}
         self.users = []
+        log.debug('Initialized {}'.format(self))
 
 
     def __repr__(self):
-        return str('<Standup channel="id:{}, name:{}">'.format(
+        return str('<{} channel="id:{}, name:{}">'.format(
+            self.__class__.__name__,
             self.channel.id, self.channel.name))
 
 
@@ -124,7 +137,7 @@ class Standup(object):
 
     def publish(self):
         log.info('Publishing standup in {}'.format(self.channel.name))
-        res = ['*Standup for {}*'.format(self.channel.name)]
+        res = [_('standup_for', standup=self)]
         [res.append(ups.display(tag)) for tag, ups in self.updates.items()]
         self.channel.send_message('\n \n'.join(res))
 
@@ -134,13 +147,23 @@ class Updates(object):
         self.done = []
         self.blocked = []
         self.goals = []
+        log.debug('Initialized {}'.format(self))
+
+
+    def __repr__(self):
+        return str('<{} done={} blocked={} goals={}>'.format(
+            self.__class__.__name__,
+            len(self.done), len(self.blocked), len(self.goals)))
+
 
     def display(self, tag):
         i = len(self.done) + len(self.blocked) + len(self.goals)
         return self._as_none(tag) if i < 1 else self._as_table(tag)
 
+
     def _as_none(self, tag):
-        return '{}: *None*'.format(tag)
+        return tag + ': ' + _('None')
+
 
     def _as_table(self, tag):
         w = TextWrapper(width=68, initial_indent='* ',
@@ -148,17 +171,18 @@ class Updates(object):
 
         d = []
         if len(self.done):
-            d.append(('Done', '\n'.join([w.fill(i) for i in self.done])))
+            d.append((_('Done'), '\n'.join([w.fill(i) for i in self.done])))
         if len(self.blocked):
-            d.append(('Blocked', '\n'.join([w.fill(i) for i in self.blocked])))
+            d.append((_('Blocked'), '\n'.join([w.fill(i) for i in self.blocked])))
         if len(self.goals):
-            d.append(('Goals', '\n'.join([w.fill(i) for i in self.goals])))
+            d.append((_('Goals'), '\n'.join([w.fill(i) for i in self.goals])))
 
         t = AsciiTable(d)
         t.inner_row_border = True
         t.outer_border = False
         t.justify_columns = {0: 'right', 1: 'left'}
-        return tag + ':\n```' + t.table + '```'
+        return tag + ':```\n' + t.table + '```'
+
 
 
 class Channel(object):
@@ -167,8 +191,13 @@ class Channel(object):
         self.id = channel_info["id"]
         self.name = channel_info["name"]
         self.members = channel_info["members"]
+        log.debug('Initialized {}'.format(self))
 
-        log.debug('Channel Initialized, ID: {} Name: {} Members: {}'.format(self.id, self.name, self.members))
+
+    def __repr__(self):
+        return str('<{} id="{}" name="{}" members="{}">'.format(
+            self.__class__.__name__,
+            self.id, self.name, self.members))
 
 
     def send_message(self, message):
@@ -181,20 +210,24 @@ class User(object):
         self.id = user_info["id"]
         self.name = user_info["name"]
         self.tag = '<@{}>'.format(self.id)
+        self.connected = False
         self.dm_channel = None
         self.standups = []
+        log.debug('Initialized {}'.format(self))
 
 
     def __repr__(self):
-        return str('<User [{}] "id:{}, name:{}, chan: {}">'.format(
-            len(self.standups), self.id, self.name, self.dm_channel))
+        return str('<{} id="{}" name="{}" channel="{}" c={}>'.format(
+            self.__class__.__name__,
+            self.id, self.name, self.dm_channel, len(self.standups)))
 
 
     def connect(self):
         res = self.slack_client.api_call('im.open', user=self.id)
+        self.connected = True
         self.dm_channel = res['channel']['id']
 
-        log.debug('User Initialized, ID: {} Name {} DM {}'.format(self.id, self.name, self.dm_channel))
+        log.debug('Connected {}'.format(self))
 
 
     def send_message(self, message):
@@ -206,7 +239,8 @@ class User(object):
 class StandupBot(object):
     def __init__(self, config):
         assert isinstance(config, ConfigParser)
-        self.token = config.get('slack', 'token')
+        self.config = config
+        self.token = self.config.get('slack', 'token')
 
         self.bot = None
         self.channels = {}
@@ -236,6 +270,7 @@ class StandupBot(object):
         if channel_object['ok'] is True:
             return Channel(channel_object['channel'], self.slack_client)
 
+        # not a channel, probably a pvt group
         group_object = self.slack_client.api_call('groups.info', channel=channel_name)
         if group_object['ok'] is True:
             return Channel(group_object['group'], self.slack_client)
@@ -266,13 +301,13 @@ class StandupBot(object):
                 for response in messages:
                     self.handle_response(response)
             else:
-                time.sleep(0.5)
+                time.sleep(0.1)
 
 
     def parse_cmd(self, text):
-        tokens = text.strip().split(' ', 1)
-        cmd = tokens[0]
-        msg = tokens[1] if len(tokens) > 1 else None
+        words = text.strip().split(' ', 1)
+        cmd = words[0]
+        msg = words[1] if len(words) > 1 else None
         return (cmd, msg)
 
 
@@ -286,22 +321,26 @@ class StandupBot(object):
         direct = True if channel_name[0] == 'D' else False
         mention = False
 
+        # pay attention to '@bot COMMAND' messages
         if cmd == self.mention_prefix:
             mention = True
             cmd, msg = self.parse_cmd(msg)
 
         # simple query/response commands
         if direct or mention:
-            if cmd == 'help':
+            if cmd in commands['help']:
                 self.slack_client.rtm_send_message(channel_name,
                     _('help', name=self.bot.name)
                     )
                 return
-            elif cmd == 'ping':
+            elif cmd in commands['ping']:
                 self.slack_client.rtm_send_message(channel_name, _('pong'))
                 return
 
         if direct and active:
+            # This is a user interacting with the bot directly.
+            # Usually, this will be someone entering or editing
+            # their status
             user = self.channels[channel_name]
             standup = user.standups[0]
 
@@ -310,7 +349,7 @@ class StandupBot(object):
             done = ups.done
             blocked = ups.blocked
             goals = ups.goals
-            stat = 'Ok'
+            stat = _('Ok')
 
             if msg and cmd in commands['done']:
                 done.append(msg)
@@ -321,13 +360,13 @@ class StandupBot(object):
             elif msg and cmd in commands['goals']:
                 goals.append(msg)
 
-            elif cmd in commands['reset'] and msg == 'done':
+            elif cmd in commands['reset'] and msg in commands['done']:
                 done.clear()
 
-            elif cmd in commands['reset'] and msg == 'blocked':
+            elif cmd in commands['reset'] and msg in commands['blocked']:
                 blocked.clear()
 
-            elif cmd in commands['reset'] and msg == 'goals':
+            elif cmd in commands['reset'] and msg in commands['goals']:
                 goals.clear()
 
             elif cmd in commands['reset'] and msg is None:
@@ -335,25 +374,26 @@ class StandupBot(object):
                 blocked.clear()
                 goals.clear()
 
+            elif cmd in commands['reset']:
+                stat = _('reset_what')
+
             elif cmd in commands['show']:
                 user.send_message(_('preview', update=ups.display(user.tag)))
-
-            elif cmd in commands['reset']:
-                stat = 'Huh?'
 
             elif cmd in commands['skip']:
                 done = []
                 blocked = []
                 goals = []
-                stat = 'Skipping'
+                stat = _('Skipping')
                 end = True
 
             elif cmd in commands['end']:
-                stat = 'Thanks'
+                stat = _('Thanks')
                 end = True
 
             else:
-                stat = 'Bad command'
+                user.send_message(_('unknown_command'))
+                return
 
             user.send_message(
                 '{}; {}: _done:_ {}, _blocked:_ {}, _goals:_ {}'.format(
@@ -377,9 +417,19 @@ class StandupBot(object):
 
         elif mention:
             if not active:
-                if cmd == 'start':
+                if cmd in commands['start']:
                     log.info('Beginning standup in {}'.format(channel_name))
                     channel = self.get_channel(channel_name)
+
+                    section = 'channel:{}'.format(channel.name)
+                    ignore_users = []
+                    if self.config.has_section(section):
+                        if self.config.has_option(section, 'ignore'):
+                            ignore_str = self.config.get(section, 'ignore')
+                            ignore_users = [u.strip() for u in
+                                    ignore_str.split(',')]
+                            log.debug('Ignore: {}'.format(ignore_users))
+
                     standup = Standup(channel)
                     self.channels.update({channel_name: standup})
                     for uid in standup.channel.members:
@@ -390,6 +440,13 @@ class StandupBot(object):
                             user = self.get_user(uid)
                             if not user:
                                 continue
+
+                        if user.name in ignore_users:
+                            log.info('Not adding {} to {}'.format(
+                                user.name, standup.channel.name))
+                            continue
+
+                        if not user.connected:
                             user.connect()
 
                         if len(user.standups) < 1:
@@ -403,7 +460,13 @@ class StandupBot(object):
 
                         user.standups.append(standup)
                         standup.add_user(user)
-                    standup.channel.send_message(_('standup_started', standup=standup))
+
+                    if standup.outstanding > 0:
+                        standup.channel.send_message(_('standup_started', standup=standup))
+                    else:
+                        standup.channel.send_message(_('standup_empty', standup=standup))
+                        log.error('Standup is empty!')
+                        self.unlink(standup)
 
                 else:
                     log.debug('bad cmd, inactive state: "{}"'.format(cmd))
@@ -412,15 +475,15 @@ class StandupBot(object):
                 # In a channel with an active standup
                 standup = self.channels[channel_name]
 
-                if cmd == 'start':
+                if cmd in commands['start']:
                     standup.channel.send_message(_('standup_already', standup=standup))
 
-                elif cmd == 'cancel':
+                elif cmd in commands['cancel']:
                     log.info('Cancelling standup in {}'.format(channel_name))
                     standup.channel.send_message(_('standup_cancelled', standup=standup))
                     self.unlink(standup)
 
-                elif cmd == 'publish':
+                elif cmd in commands['publish']:
                     standup.publish()
                     self.unlink(standup)
 
@@ -471,15 +534,31 @@ class StandupBot(object):
         self.channels.pop(standup.channel.id)
 
 
-# Startup
-if __name__ == '__main__':
+
+def main():
     import argparse
-    # TODO - args
 
     here = os.path.dirname(__file__)
-    conf_file = os.path.abspath("{}/{}".format(here, 'conf.ini'))
+    default_file = os.path.abspath("{}/{}".format(here, 'conf.ini'))
+
+    parser = argparse.ArgumentParser(description='SLack Agile Not Dangerously Evil Robot')
+
+    log_group = parser.add_mutually_exclusive_group()
+    log_group.add_argument('-v', '--verbose', action='count', default=0,
+            dest='verbosity')
+    log_group.add_argument('-q', '--quiet', action='count', default=0,
+            dest='quiet')
+    parser.add_argument('config_file', action='store', type=open,
+            default=default_file)
+    args = parser.parse_args()
+
+    verbosity = min(2, args.verbosity)
+    quiet = min(2, args.quiet)
+    log_level = 30 - (verbosity*10) + (quiet*10) # yeah, I know
+    logging.basicConfig(level=log_level)
+
     config = ConfigParser()
-    config.read(conf_file)
+    config.read_file(args.config_file)
     bot = StandupBot(config)
 
     while True:
@@ -489,3 +568,10 @@ if __name__ == '__main__':
             sys.exit(0)
         except Exception as e:
             logging.exception('Bot failed')
+    sys.exit(1)
+
+
+
+# Startup
+if __name__ == '__main__':
+    main()
