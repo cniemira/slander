f191fd71b717f8f612e8600dd75208a415ce884a
Chris Niemira <chris.niemira@teamaol.com>
2016-10-31 13:11:07 -0400

    logging tweaks, added echo, unfurl links

--- slander/bot.py
+++ slander/bot.py
@@ -12,6 +12,7 @@ from terminaltables import AsciiTable
 from .lang import commands, _
 
 log = logging.getLogger(__name__)
+log_form = '%(asctime)-15s %(module)s %(levelname)s %(message)s'
 
 
 
@@ -24,7 +25,7 @@ class Standup(object):
         self.outstanding = 0
         self.updates = {}
         self.users = []
-        log.debug('Initialized {}'.format(self))
+        log.debug('initialized:"{}"'.format(self))
 
 
     def __repr__(self):
@@ -41,10 +42,11 @@ class Standup(object):
 
 
     def publish(self):
-        log.info('Publishing standup in {}'.format(self.channel.name))
+        log.info('publish; channel:"{}"'.format(self.channel.name))
         res = [_('standup_for', standup=self)]
         [res.append(ups.display(tag)) for tag, ups in self.updates.items()]
         self.channel.send_message('\n \n'.join(res))
+
 
 
 class Updates(object):
@@ -54,7 +56,7 @@ class Updates(object):
         self.done = []
         self.blocked = []
         self.goals = []
-        log.debug('Initialized {}'.format(self))
+        log.debug('initialized:"{}"'.format(self))
 
 
     def __repr__(self):
@@ -100,7 +102,7 @@ class Channel(object):
         self.id = channel_info["id"]
         self.name = channel_info["name"]
         self.members = channel_info["members"]
-        log.debug('Initialized {}'.format(self))
+        log.debug('initialized:"{}"'.format(self))
 
 
     def __repr__(self):
@@ -125,7 +127,7 @@ class User(object):
         self.connected = False
         self.dm_channel = None
         self.standups = []
-        log.debug('Initialized {}'.format(self))
+        log.debug('initialized:"{}"'.format(self))
 
 
     def __repr__(self):
@@ -139,7 +141,7 @@ class User(object):
         self.connected = True
         self.dm_channel = res['channel']['id']
 
-        log.debug('Connected {}'.format(self))
+        log.debug('connected:"{}"'.format(self))
 
 
     def send_message(self, message):
@@ -166,24 +168,26 @@ class StandupBot(object):
 
         self.last_ping = 0
         self.keepalive_timer = 3
-        log.debug('Initialized {}'.format(self))
+        log.debug('initialized:"{}"'.format(self))
 
 
     def __repr__(self):
+        if not self.bot:
+            return str('<{}>'.format(self.__class__.__name__))
         return str('<{} bot_name="{}" n_users={} n_channels={}>'.format(
             self.__class__.__name__,
             self.bot.name, len(self.users), len(self.channels)))
 
 
     def connect(self):
-        log.debug('Connecting')
+        log.debug('connecting')
         self.slack_client = SlackClient(self.token)
         self.slack_client.rtm_connect()
         self.bot = self.slack_client.server.users.find(
             self.slack_client.server.username
             )
         self.mention_prefix = '<@{}>'.format(self.bot.id)
-        log.info('Connected as {} [{}]'.format(self.bot.name, self.mention_prefix))
+        log.info('connected; name:"{}", prefix:"{}"'.format(self.bot.name, self.mention_prefix))
 
 
     def get_channel(self, channel_name):
@@ -227,20 +231,35 @@ class StandupBot(object):
 
 
     def parse_cmd(self, text):
-        words = text.strip().split(' ', 1)
-        cmd = words[0]
-        msg = words[1] if len(words) > 1 else None
+        p = text.strip().split(' ', 1)
+        cmd = p[0]
+        msg = self.unfurl(p[1]) if len(p) > 1 else None
         return (cmd, msg)
+
+
+    def unfurl(self, text):
+        while text.count('<') > 0:
+            assert text.count('<') == text.count('>')
+            s = text.index('<') + 1
+            e = text.index('>')
+            sub = text[s:e]
+            old = '<{}>'.format(sub)
+            new = sub.split('|', 1)[-1]
+            text = text.replace(old, new)
+            log.debug('unfurlled; old:"{}", new:"{}"'.format(old, new))
+        return text
+
 
 
     def handle_message(self, channel_name, message):
         # this is hideous and should probably be rewritten, but
         # I'm lazy and it works
         if not type(message) is str and len(message) > 1:
-            log.info('Bad message in {}'.format(channel_name))
+            log.info('bad message; channel:"{}"'.format(channel_name))
             return
 
         cmd, msg = self.parse_cmd(message)
+        log.debug('parsed; cmd:"{}", msg:"{}"'.format(cmd, msg))
         active = True if channel_name in self.channels else False
         direct = True if channel_name[0] == 'D' else False
         mention = False
@@ -253,11 +272,16 @@ class StandupBot(object):
 
         # simple query/response commands
         if direct or mention:
-            if cmd in commands['help']:
+            if cmd in commands['echo']:
+                self.slack_client.rtm_send_message(channel_name, msg)
+                return
+
+            elif cmd in commands['help']:
                 self.slack_client.rtm_send_message(channel_name,
                     _('help', name=self.bot.name)
                     )
                 return
+
             elif cmd in commands['ping']:
                 self.slack_client.rtm_send_message(channel_name, _('pong'))
                 return
@@ -316,17 +340,18 @@ class StandupBot(object):
                 end = True
 
             else:
-                user.send_message(_('unknown_command'))
+                user.send_message(_('unknown_cmd'))
                 return
 
-            user.send_message(
-                '{}; {}: _done:_ {}, _blocked:_ {}, _goals:_ {}'.format(
-                    stat, standup.channel.name,
-                    len(done), len(blocked), len(goals)))
+            user.send_message(_('status_response',
+                status=stat, standup=standup,
+                n_done=len(done), n_blocked=len(blocked), n_goals=len(goals)
+                ))
 
             if end:
-                log.debug('{} is done with {}'.format(user.name,
-                    standup.channel.name))
+                log.debug('done; user:"{}", channel:"{}"'.format(
+                    user.name, standup.channel.name
+                    ))
                 standup.channel.send_message(_('sat_down', user=user))
                 standup.outstanding -= 1
 
@@ -342,7 +367,7 @@ class StandupBot(object):
         elif mention:
             if not active:
                 if cmd in commands['start']:
-                    log.info('Beginning standup in {}'.format(channel_name))
+                    log.info('begin; channel:"{}"'.format(channel_name))
                     channel = self.get_channel(channel_name)
 
                     section = 'channel:{}'.format(channel.name)
@@ -352,7 +377,7 @@ class StandupBot(object):
                             ignore_str = self.config.get(section, 'ignore')
                             ignore_users = [u.strip() for u in
                                     ignore_str.split(',')]
-                            log.debug('Ignore: {}'.format(ignore_users))
+                            log.debug('ignore:"{}"'.format(ignore_users))
 
                     standup = Standup(channel)
                     self.channels.update({channel_name: standup})
@@ -366,7 +391,7 @@ class StandupBot(object):
                                 continue
 
                         if user.name in ignore_users:
-                            log.info('Not adding {} to {}'.format(
+                            log.info('pass; user:"{}", channel:"{}"'.format(
                                 user.name, standup.channel.name))
                             continue
 
@@ -391,11 +416,11 @@ class StandupBot(object):
                     else:
                         standup.channel.send_message(
                             _('standup_empty', standup=standup))
-                        log.error('Standup is empty!')
+                        log.error('empty; channel:{}'.format(channel_name))
                         self.unlink(standup)
 
                 else:
-                    log.debug('bad cmd, inactive state: "{}"'.format(cmd))
+                    log.debug('unknown cmd; state="inactive", cmd:"{}"'.format(cmd))
 
             else:
                 # In a channel with an active standup
@@ -406,7 +431,7 @@ class StandupBot(object):
                             _('standup_already', standup=standup))
 
                 elif cmd in commands['cancel']:
-                    log.info('Cancelling standup in {}'.format(channel_name))
+                    log.info('cancel; channel:"{}"'.format(channel_name))
                     standup.channel.send_message(
                             _('standup_cancelled', standup=standup))
                     self.unlink(standup)
@@ -417,7 +442,7 @@ class StandupBot(object):
 
                 else:
                     standup.channel.send_message(_('unknown_cmd'))
-                    log.debug('bad cmd, active state: "{}"'.format(cmd))
+                    log.debug('unknown cmd; state="active", cmd:"{}"'.format(cmd))
 
 
     def handle_response(self, response):
@@ -425,36 +450,36 @@ class StandupBot(object):
             if "message" in response["type"]:
                 try:
                     age = time.time() - float(response["ts"])
-                    if age > 10.0:
-                        log.warn('Skipping {}s old response: "{}"'.format(
+                    if age > 30.0:
+                        log.warn('too old; age:"{}", res:"{}"'.format(
                             age, str(response)))
                         return
 
-                    log.debug(response)
+                    log.debug('handling; res:"{}"'.format(response))
                     self.handle_message(response["channel"], response["text"])
                 except Exception as e:
-                    logging.exception('Message handling failure.')
+                    logging.exception('handling failure')
 
 
     def sit_down(self, user, standup):
         if standup in user.standups:
-            log.debug('seating {}/{} in {}'.format(user.name,
-                user.dm_channel, standup.channel.name))
+            log.debug('seating; user:"{}", dm:"{}", channel:"{}"'.format(
+                user.name, user.dm_channel, standup.channel.name))
             user.standups.pop(user.standups.index(standup))
 
             if len(user.standups) < 1:
-                log.debug('done talking to {}'.format(user.name))
+                log.debug('done; user:"{}"'.format(user.name))
                 self.channels.pop(user.dm_channel)
                 self.users.pop(user.id)
 
             else:
-                log.debug('alert {} about next standup'.format(user.name))
+                log.debug('next standup; user:"{}"'.format(user.name))
                 next_standup = user.standups[0]
                 user.send_message(_('next', standup=next_standup))
 
 
     def unlink(self, standup):
-        log.debug('unlink {}'.format(standup.channel.name))
+        log.debug('unlink; channel:"{}"'.format(standup.channel.name))
         for user in standup.users:
             if user.dm_channel in self.channels:
                 user.send_message(_('standup_ended', standup=standup))
@@ -465,6 +490,8 @@ class StandupBot(object):
 
 def main():
     import argparse
+        
+    from .conf import DEFAULT_CONFIG
 
     here = os.path.dirname(__file__)
     default_file = os.path.abspath("{}/{}".format(here, 'conf.ini'))
@@ -484,9 +511,10 @@ def main():
     verbosity = min(2, args.verbosity)
     quiet = min(2, args.quiet)
     log_level = 30 - (verbosity*10) + (quiet*10)
-    logging.basicConfig(level=log_level) # yeah, I know
+    logging.basicConfig(level=log_level, format=log_form) # yeah, I know
 
     config = ConfigParser()
+    config['DEFAULT'] = DEFAULT_CONFIG
     config.read_file(args.CONFIG_FILE)
     bot = StandupBot(config)
 
@@ -496,7 +524,7 @@ def main():
         except KeyboardInterrupt:
             sys.exit(0)
         except Exception as e:
-            logging.exception('Bot failed')
+            logging.exception('bot failed')
 
     sys.exit(1)
 
